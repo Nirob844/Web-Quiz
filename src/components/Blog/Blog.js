01eaabcbc2cdd65925be4a57c68940e8eb2f83f4
Nirob Hasan <nirob427417@gmail.com>
2023-01-10 19:05:46 +0600

    done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,6 +4,36 @@ const Blog = () => {
     return (
         <div>
             <h1>blog</h1>
+            <div className='m-auto w-2/4 my-10 bg-slate-200 rounded-2xl drop-shadow-2xl'>
+                <h1 className='text-3xl my-3'>
+                    what is purpose of react router?
+                </h1>
+                <p className='p-3'>
+                    The Router in React JS is primarily used to create Single Page Web Apps. In the application, React Router is utilized to define various routes. When a user enters a URL into your browser and the URL route equals one of several 'pathways' as in the router folder, the user is sent to that route.
+                </p>
+            </div>
+            <div className='m-auto w-2/4 my-10 bg-slate-200 rounded-2xl drop-shadow-2xl'>
+                <h1 className='text-3xl my-3'>
+                    how does context api work?
+                </h1>
+                <p className='p-3'>
+                    React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
+                </p>
+            </div>
+            <div className='m-auto w-2/4 my-10 bg-slate-200 rounded-2xl drop-shadow-2xl'>
+                <h1 className='text-3xl my-3'>
+                    React JS useRef Hook
+                </h1>
+                <p className='p-3'>
+                    The useRef hook is the new addition in React 16.8. Before proceeding to this article there is a prerequisite to know about the ref in react.
+                    The useRef is a hook that allows to directly create a reference to the DOM element in the functional component.
+
+                    Syntax:
+
+                    const refContainer = useRef(initialValue);
+                    The useRef returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component.
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { ToastContainer, toast } from 'react-toastify';
@@ -8,21 +8,22 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ qst }) => {
     const { question, options, correctAnswer, } = qst;
-    const handelCheckBox = () => {
-        const correct = correctAnswer;
-        const option = options.find(opt => opt === correct);
-        if (option) {
-            console.log(option)
+    const [value, setValue] = useState();
+    const handleClick = (e) => {
+        setValue(value.concat(e.target));
+        console.log(value)
+
+        if (value === correctAnswer) {
+            console.log('match')
             alert("right");
-            const notify = () => toast("Wow so easy!");
+            //const notify = () => toast("Wow so easy!");
         }
-        else if (!option) {
-            console.log(option)
-            const notify = () => toast("Wrong answer")
+        else {
+            console.log('not match')
+            //  const notify = () => toast("Wrong answer")
             alert("wrong");
-
-
         }
+
     }
 
     return (
@@ -36,16 +37,16 @@ const Question = ({ qst }) => {
                 </div>
 
                 <div className=' text-xl grid sm:grid-cols-2 md:grid-cols-2 '>
-                    <button onClick={handelCheckBox} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex'>
+                    <button onClick={handleClick} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex'>
                         <div><input type="checkbox" className="checkbox" />
                         </div> <p className='ml-2'>{options[0]}</p></button>
-                    <button onClick={handelCheckBox} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex'>
+                    <button onClick={handleClick} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex'>
                         <div><input type="checkbox" className="checkbox" />
                         </div> <p className='ml-2'>{options[1]}</p></button>
-                    <button onClick={handelCheckBox} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex '>
+                    <button onClick={handleClick} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex '>
                         <div><input type="checkbox" className="checkbox" />
                         </div> <p className='ml-2'>{options[2]}</p></button>
-                    <button onClick={handelCheckBox} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex'>
+                    <button onClick={handleClick} className='border-solid border-2 border-sky-500 rounded-md px-4 py-2 mx-4 my-2 flex'>
                         <div><input type="checkbox" className="checkbox" />
                         </div> <p className='ml-2'>{options[3]}</p></button>
                 </div>
